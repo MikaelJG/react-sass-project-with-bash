7e4562fd4d23b6f47e330b1817430050448f730c
Mikael Gonsalves <mjgonsalves@protonmail.com>
2022-09-30 16:47:38 -0400

    working append to index.html and style.css

--- jul_loop_idea/jul_script_idea.sh
+++ jul_loop_idea/jul_script_idea.sh
@@ -3,23 +3,15 @@ cd wagon_ui_components
 for f in *; do
     if [ -d "$f" ]; then
                 read -p "
-Would you like a $f component?" yn
+Would you like a $f component in your index.html?" yn
                             case $yn in
                                 [Yy]* )
-                                    chmod +x $f.sh;;
-                                [Nn]* ) echo "in no condition" ;;
-                                * ) echo "in else condition";;
+                                    chmod +x $f.sh;
+                                    ./$f.sh;;
+                                [Nn]* ) echo "$f was not added to you html" ;;
+                                * ) echo "Please answer with Yy [yes] or Nn [no]";;
                             esac
 
     fi
 done
-
-
-#echo "do you want this $COMPONENT"?
-#    if yes
-#        echo HTML + CSS << append
-#    if no
-#        break;
-#    end
-
 
